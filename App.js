884bbeca770f8e4bdfb4267c93bb767eb6e5e827
Trevor <Trevorauten@gmail.com>
2022-03-18 17:37:48 -0500

    Initial home.js additions
    Began setting up and styling home.js. Created navbar.js for future use

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from "./home"
+import navbar from "./navbar"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  //return (
+    //<View style={styles.container}>
+    //  <Text>Hello World!</Text>
+    //  <StatusBar style="auto" />
+    //</View>
+  //);
+  return <Home />;
 }
 
 const styles = StyleSheet.create({
---
+++ navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const Navbar = () => {
+    return (
+        <View>
+            <Text>Navbar Here</Text>
+            </View>
+    );
+}
+
+
+  export default Navbar;
