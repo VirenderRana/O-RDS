d0c90dfdf836fdcdc7944559e165eee758fb08d1
Trevor <Trevorauten@gmail.com>
2022-03-18 14:10:02 -0500

    Revert "Test"
    This reverts commit 27b3515a2dbfecbd0109162bea7228d8d991bd33.

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-// testing
 export default function App() {
   return (
     <View style={styles.container}>
