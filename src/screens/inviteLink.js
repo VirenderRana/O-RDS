e0b098a3ba8806312ef3f17cf543ac2d9d5b1e15
Trevor <Trevorauten@gmail.com>
2022-03-25 17:00:05 -0500

    Contact basics
    The basic framework for the contacts. Struggling to have .getAll() return the json values

--- src/screens/inviteLink.js
+++ src/screens/inviteLink.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Dimensions, ScrollView } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Button, Dimensions, ScrollView, FlatList, PermissionsAndroid } from 'react-native';
+import Contacts from 'react-native-contacts';
+import {Contact} from '../utils/contact';
+
 
 const InviteLink = () => {
+  
+  let [contacts, setContacts] = useState([]);
+  console.log(Contacts)
+  useEffect(() => {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+      {
+        'title': 'Contacts',
+        'message': 'This app would like to view your contacts.',
+        'buttonPositive': 'Please accept'
+      }
+    ).then(() => {
+      console.log("1")
+      Contacts.getAll().then(contacts => {
+        console.log("2");
+        setContacts(contacts);
+          
+    })
+      })
+        });
+
+  const keyExtractor = (item, idx) => {
+    return item?.recordID?.toString() || idx.toString();
+  };
+
+  const renderItem = ({item, index}) => {
+    return <Contact contact={item} />;
+  };
+
   return (
-    <Text>Invite here</Text>
+    <FlatList data={contacts} renderItem={renderItem} keyExtractor={keyExtractor} /> 
+      
   )
+      
+    
+  
 }
 
 export default InviteLink
